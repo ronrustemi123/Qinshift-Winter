f99ea474c0eec2a5b167ff331244267c62b74746
ronrustemi123 <ronrustemii@gmail.com>
2025-01-10 22:50:40 +0100

    final i think

--- Hangman/app.js
+++ Hangman/app.js
@@ -1,39 +1,58 @@
-const letterA = document.querySelector("#a");
-const letterB = document.querySelector("#b");
-const letterC = document.querySelector("#c");
-const letterD = document.querySelector("#d");
-const letterE = document.querySelector("#e");
-const letterF = document.querySelector("#f");
-const letterG = document.querySelector("#g");
-const letterH = document.querySelector("#h");
-const letterI = document.querySelector("#i");
-const letterJ = document.querySelector("#j");
-const letterK = document.querySelector("#k");
-const letterL = document.querySelector("#l");
-const letterM = document.querySelector("#m");
-const letterN = document.querySelector("#n");
-const letterO = document.querySelector("#o");
-const letterP = document.querySelector("#p");
-const letterQ = document.querySelector("#q");
-const letterR = document.querySelector("#r");
-const letterS = document.querySelector("#s");
-const letterT = document.querySelector("#t");
-const letterU = document.querySelector("#u");
-const letterV = document.querySelector("#v");
-const letterW = document.querySelector("#w");
-const letterX = document.querySelector("#x");
-const letterY = document.querySelector("#y");
-const letterZ = document.querySelector("#z");
-
 const hintBtn = document.querySelector("#hint-btn");
 const againBtn = document.querySelector("#again-btn");
+
+const hintBox = document.querySelector('.hint-box')
 
 const wordSect = document.querySelector('.word-sect')
 const livesSpan = document.querySelector('.lives-span')
 
-const words = ['australia', 'afganistan', 'germany', 'united kingdom', 'norway', 'ireland', 'china', 'japan', 'mexico', 'azerbaijan']
-let lives = 6
+const words = [
+    {
+        country: 'australia',
+        hint: 'kangaroo'
+    },
+    {
+        country: 'afghanistan',
+        hint: 'kabul'
+    },
+    {
+        country: 'germany',
+        hint: 'world war II'
+    },
+    {
+        country: 'united kingdom',
+        hint: 'big ben'
+    },
+    {
+        country: 'norway',
+        hint: 'fjords'
+    },
+    {
+        country: 'ireland',
+        hint: 'leprechauns'
+    },
+    {
+        country: 'china',
+        hint: 'great wall'
+    },
+    {
+        country: 'japan',
+        hint: 'mount fuji'
+    },
+    {
+        country: 'mexico',
+        hint: 'tacos'
+    },
+    {
+        country: 'georgia',
+        hint: 'wine inventor'
+    }
+];
+
 const guesses = []
+let lives = 6
+livesSpan.textContent = lives
+
 const letters = [
     "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
     "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
@@ -47,13 +66,9 @@ const getRandomCountry = () => {
     return words[index];
 };
 const randomCountry = getRandomCountry()
-let randomCountryIndexes = []
-for (let i = 0; i < randomCountry.length; i++) {
-    randomCountryIndexes.push(i)
-}
 
 const printDashes = () => {
-    const countryLetters = randomCountry.split('')
+    const countryLetters = randomCountry.country.split('')
     for (let i = 0; i < countryLetters.length; i++) {
         const pTag = document.createElement('p')
         pTag.textContent = '_'
@@ -63,26 +78,62 @@ const printDashes = () => {
 }
 printDashes()
 
-const randomCountryArr = randomCountry.split('')
+const randomCountryArr = randomCountry.country.split('')
 console.log(randomCountry)
-for (const letter of letters) {
-    const letterElement = document.querySelector(`#${letter}`);
 
-    letterElement.addEventListener('click', () => {
-        if (randomCountryArr.includes(letter.toLowerCase())) {
-            const underScore = document.querySelectorAll(`.${letter}`)
-            if (underScore.length === 1) {
-                if (underScore[0].className == letter) {
-                    underScore[0].textContent = letter
+const checkLetter = () => {
+
+    for (const letter of letters) {
+        const letterElement = document.querySelector(`#${letter}`);
+        const underScore = document.querySelectorAll(`.${letter}`)
+
+        letterElement.addEventListener('click', () => {
+            if (randomCountryArr.includes(letter.toLowerCase())) {
+                if (underScore.length === 1) {
+                    if (underScore[0].className == letter) {
+                        underScore[0].textContent = letter
+                        guesses.push(letter)
+                    }
+                } else {
+                    if (underScore[0].className == letter) {
+                        underScore.forEach(el => el.textContent = letter)
+                        guesses.push(letter)
+                    }
                 }
             } else {
-                if (underScore[0].className == letter) {
-                    underScore.forEach(el => el.textContent = letter)
+                lives -= 1
+                if (lives === 0) {
+                    alert('Mission Failed. Your country was: ' + randomCountry)
+                    for (const letter of letters) {
+                        const letterElement = document.querySelector(`#${letter}`);
+                        letterElement.className = 'disabled'
+                    }
                 }
+                livesSpan.textContent = lives
             }
-        } else {
-            lives -= 1
-        }
-    });
+            const sortedGuess = guesses.sort().join('')
+            const matchingGuess = words.find(el => el.country.split('').sort().join('') === sortedGuess)
+            if (matchingGuess) {
+                for (const letter of letters) {
+                    const letterElement = document.querySelector(`#${letter}`);
+                    letterElement.className = 'disabled'
+                }
+                alert("You Won!")
+
+            }
+        });
+    }
+
 }
+
+checkLetter()
+
+againBtn.addEventListener('click', () => {
+    location.reload()
+})
+
+hintBtn.addEventListener('click', () => {
+    const hintText = document.querySelector('.hint-box-text')
+    hintText.textContent = randomCountry.hint
+})
 
